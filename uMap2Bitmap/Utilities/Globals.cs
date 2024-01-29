c6185bdac95ddb8b63b2d634fb5f700c83772a51
Alex Virlan <dj.3agl3@gmail.com>
2024-01-29 15:19:56 +0200

    Globals cleanup

--- uMap2Bitmap/Utilities/Globals.cs
+++ uMap2Bitmap/Utilities/Globals.cs
@@ -10,16 +10,6 @@ namespace uMap2Bitmap.Utilities
     public static class Globals
     {
         public static List<string>? Args = null;
-        //public static frmMain? FrmMain = null;
-        //public static frmBrowser? FrmBrowser = null;
-
-        //public delegate void BrowserReady();
-        //public static event BrowserReady OnBrowserReady;
-
-        //public static event EventHandler ProcessCompleted;
-
-        public static List<string> PolysProps = new List<string>();
-
         public static Dictionary<string, Dictionary<string, List<string>>> CustomPropsStats;
 
     }
