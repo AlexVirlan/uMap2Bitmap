81bcecf0366dda93726a171b7425e4779c671dd1
Alex Virlan <dj.3agl3@gmail.com>
2024-01-31 23:34:31 +0200

    Finishing the "Check in polygons" function

--- uMap2Bitmap/Forms/frmAddTag.cs
+++ uMap2Bitmap/Forms/frmAddTag.cs
@@ -19,6 +19,7 @@ namespace uMap2Bitmap.Forms
         #region Variables
         public Tag? Tag;
         private DataGridView _dgvGlobalTags;
+        private string _NL = Environment.NewLine;
         #endregion
 
         public frmAddTag(ref DataGridView dataGridView)
@@ -61,7 +62,7 @@ namespace uMap2Bitmap.Forms
 
             if (Globals.CustomPropsStats.ContainsKey(tagName))
             {
-                return (false, "There are some polygons that have properties with this name. Therefore, this tag name can't be used." + Environment.NewLine + 
+                return (false, "There are some polygons that have properties with this name. Therefore, this tag name can't be used." + _NL +
                     "Press the 'Check in polygons' label to see where it's used.");
             }
 
@@ -92,7 +93,7 @@ namespace uMap2Bitmap.Forms
 
         private void moreInfoToolStripMenuItem_Click(object sender, EventArgs e)
         {
-            MessageBox.Show(this, $"The format is {{X}} where X is the dynamic value name. They are case-insensitive and their names are predetermined. Select one of them to insert it as a tag value. {Environment.NewLine.Repeat()}The 'custom integers' is a special one because you can specify the min and max integers, the format is {{{{rnd:X-Y}}}} where X is the min integer and Y is the max integer, eg.: {{{{rnd:10-100}}}}", "Dynamic values info", MessageBoxButtons.OK, MessageBoxIcon.Information);
+            MessageBox.Show(this, $"The format is {{X}} where X is the dynamic value name. They are case-insensitive and their names are predetermined. Select one of them to insert it as a tag value. {_NL.Repeat()}The 'custom integers' is a special one because you can specify the min and max integers, the format is {{{{rnd:X-Y}}}} where X is the min integer and Y is the max integer, eg.: {{{{rnd:10-100}}}}", "Dynamic values info", MessageBoxButtons.OK, MessageBoxIcon.Information);
         }
 
         private void MenuAddDynamicValue(object sender, EventArgs e)
@@ -119,22 +120,31 @@ namespace uMap2Bitmap.Forms
                 string polys = polysCount == 1 ? "one polygon" : $"{polysCount} polygons";
                 string layers = layersCount == 1 ? "one layer" : $"{layersCount} layers";
 
-                DialogResult result = MessageBox.Show(this, $"This name IS used in {polys}, from {layers}." +
-                    Environment.NewLine + "Do you want to see exactly where?",
+                DialogResult drShowDetails = MessageBox.Show(this, $"This property name IS used in {polys}, from {layers}." + _NL +
+                    "Do you want to see exactly where?" + _NL.Repeat() +
+                    "Note: hold down CTRL while clicking yes to see the data in JSON format. Otherwise, it will be in a more friendly human-readable format.",
                     "Global tag name check", MessageBoxButtons.YesNo, MessageBoxIcon.Warning);
-                if (result == DialogResult.Yes)
+                if (drShowDetails == DialogResult.Yes)
                 {
-                    // TO DO: compute the result & open it in notepad
                     Dictionary<string, List<string>> tagUsageDetails = Globals.CustomPropsStats[tagName].Where(w => w.Value.Count > 0).ToDictionary();
-                    string response = JsonConvert.SerializeObject(tagUsageDetails, Formatting.Indented);
-                    Helpers.ShowNotepadMessage(response);
-                    // TO DO: submenu - show in human-readable form, or JSON
-
+                    if (ModifierKeys.HasFlag(Keys.Control))
+                    {
+                        Helpers.ShowNotepadMessage(JsonConvert.SerializeObject(tagUsageDetails, Formatting.Indented), "Tag name usage");
+                    }
+                    else
+                    {
+                        string result = @$"The property ""{tagName}"" is used in:" + _NL.Repeat();
+                        foreach (KeyValuePair<string, List<string>> kvpLP in tagUsageDetails)
+                        {
+                            result += $"• {kvpLP.Key}" + _NL + string.Join(_NL, kvpLP.Value) + _NL.Repeat();
+                        }
+                        Helpers.ShowNotepadMessage(result, "Tag name usage");
+                    }
                 }
             }
             else
             {
-                MessageBox.Show(this, $"This name is NOT used in any polygon's properties. {Environment.NewLine}You're free to use it.",
+                MessageBox.Show(this, $"This name is NOT used in any polygon's properties. {_NL}You're free to use it.",
                     "Global tag name check", MessageBoxButtons.OK, MessageBoxIcon.Information);
             }
         }
